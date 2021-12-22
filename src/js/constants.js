07aad6481a7cdfb8f0bc0a034d13cc67c654543a
Felquis G <frgformenton@gmail.com>
2021-12-22 20:40:47 -0300

    Update dependencies 2021 edition

--- src/js/constants.js
+++ src/js/constants.js
@@ -4,4 +4,4 @@
     constants.js stores values which must be the same throw out the application
 */
 
-export const HELLO = 'Hello World'
+export const HELLO = 'Hello World 2'
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,6 @@
 import { HELLO } from 'constants'
+
+const h1 = document.querySelector('h1')
+
+h1.textContent = HELLO
+
